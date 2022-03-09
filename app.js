a384a4b392b8af6a0607d4bdc877802085a1514a
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2022-03-09 13:48:54 -0500

    Add mongoose connection.

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://ip-172-31-13-109.us-west-2.compute.internal:27017/test', (err) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log('Db connected!');
+    }
+});
+
 const app = express();
 const port = 3000;
 
